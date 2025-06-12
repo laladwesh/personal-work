f30c6c8ca5e6763399338c585004c5471df35d98
laladwesh <guptaavinash302@gmail.com>
2025-06-12 14:31:53 +0530

    section changes

--- frontend/src/components/service/ServiceSection3.jsx
+++ frontend/src/components/service/ServiceSection3.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import { useContext } from "react";
 import AccordionWithOptions from "../Accordion";
 import { UserContext } from "../../context/user";
 import { useNavigate } from "react-router-dom";
