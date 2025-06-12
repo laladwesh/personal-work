e126f6c3e55c46f44e5958820cb062c039072d11
laladwesh <guptaavinash302@gmail.com>
2025-06-12 14:33:26 +0530

    all serivces page minor changes fix in UI

--- frontend/src/pages/AllServicePage.jsx
+++ frontend/src/pages/AllServicePage.jsx
@@ -1,11 +1,10 @@
-import React from "react";
 import { useNavigate } from "react-router-dom";
 
 const AllServicePage = () => {
   const navigate = useNavigate();
 
   const goBack = () => {
-    navigate(-1); // This navigates to the previous page in the history stack
+    navigate(-1); 
   };
   return (
     <div className="px-4 md:px-16 lg:px-28 bg-gray-100 font-montserrat">
