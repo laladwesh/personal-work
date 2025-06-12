a8f68707a892cfb8e67a437eb66cac85083b59d8
laladwesh <guptaavinash302@gmail.com>
2025-06-12 14:34:43 +0530

    service pagefixes

--- frontend/src/pages/ServicePage.jsx
+++ frontend/src/pages/ServicePage.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useRef } from "react";
+import { useContext, useRef } from "react";
 import { UserContext } from "../context/user";
 import ServiceSection1 from "../components/service/ServiceSection1";
 import ServiceSection2 from "../components/service/ServiceSection2";
@@ -8,24 +8,25 @@ import ServiceSection3b from "../components/service/ServiceSection3b";
 import { useParams } from "react-router-dom";
 import { useOrder } from "../context/order";
 
-
 const ServicePage = () => {
   const section3Ref = useRef(null); // Create a reference for ServiceSection3
   const userContext = useContext(UserContext);
-  const{ setAddress} = useOrder();
-  const {id} = useParams();
-      setAddress(id);  
+  const { setAddress } = useOrder();
+  const { id } = useParams();
+  setAddress(id);
   const scrollToSection3 = () => {
     section3Ref.current.scrollIntoView({ behavior: "smooth" }); // Scroll to ServiceSection3 smoothly
   };
   return (
     <div>
-        <ServiceSection1 onKnowMoreClick={scrollToSection3}/>
-        <ServiceSection2/>
-        <div ref={section3Ref}> {/* Attach the reference to ServiceSection3 */}
-        {userContext.user?<ServiceSection3b />:<ServiceSection3/>}
+      <ServiceSection1 onKnowMoreClick={scrollToSection3} />
+      <ServiceSection2 />
+      <div ref={section3Ref}>
+        {" "}
+        {/* Attach the reference to ServiceSection3 */}
+        {userContext.user ? <ServiceSection3b /> : <ServiceSection3 />}
       </div>
-        <ServiceSection4/>
+      <ServiceSection4 />
     </div>
   );
 };
